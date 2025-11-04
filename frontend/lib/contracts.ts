3809303f0b47c4e5cff401887225ea6ec730d2fb
AlmogLach <almog857@icloud.com>
2025-11-04 23:39:06 +0200

    Update contract addresses

--- frontend/lib/contracts.ts
+++ frontend/lib/contracts.ts
@@ -1,6 +1,6 @@
 export const addresses = {
-  core: (process.env.NEXT_PUBLIC_CORE_ADDRESS || '0x83438B43dAFfe80e67Ca7204eaEe4674d0EF6BBe') as `0x${string}`,
-  usdc: (process.env.NEXT_PUBLIC_USDC_ADDRESS || '0x0E5cB1F812ce0402fdF0c9cee2E1FE3BF351a827') as `0x${string}`,
+  core: (process.env.NEXT_PUBLIC_CORE_ADDRESS || '0x02344e92a5389D3cB0100844eE262CA10818aC6e') as `0x${string}`,
+  usdc: (process.env.NEXT_PUBLIC_USDC_ADDRESS || '0x4B40348623C230F666780aa626a70cf5C95D908a') as `0x${string}`,
   admin: (process.env.NEXT_PUBLIC_ADMIN_ADDRESS || '0xbd0e87A678f3D53a27D1bb186cfc8fd465433554') as `0x${string}`,
 };
 
